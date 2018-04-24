df8c73a0be99d4a18892470d05e74a4a3e26f87f
Christian Lipphardt <christian.lipphardt@googlemail.com>
2018-04-24 14:34:25 +0200

    chore(config/capability): allow to enable custom capability types (like audit etc.)

--- files/groovy/setup_capability.groovy
+++ files/groovy/setup_capability.groovy
@@ -6,8 +6,15 @@ import org.sonatype.nexus.internal.capability.DefaultCapabilityRegistry
 
 parsed_args = new JsonSlurper().parseText(args)
 
-parsed_args.capability_properties['headerEnabled'] = parsed_args.capability_properties['headerEnabled'].toString()
-parsed_args.capability_properties['footerEnabled'] = parsed_args.capability_properties['footerEnabled'].toString()
+if (parsed_args.capability_typeId == "rapture.branding") {
+    parsed_args.capability_properties['headerEnabled'] = parsed_args.capability_properties['headerEnabled'].toString()
+    parsed_args.capability_properties['footerEnabled'] = parsed_args.capability_properties['footerEnabled'].toString()
+}
+// init if empty
+if (!parsed_args.capability_properties) {
+  parsed_args.capability_properties = [:]
+}
+
 
 def capabilityRegistry = container.lookup(DefaultCapabilityRegistry.class.getName())
 def capabilityType = CapabilityType.capabilityType(parsed_args.capability_typeId)
@@ -17,12 +24,12 @@ DefaultCapabilityReference existing = capabilityRegistry.all.find { CapabilityRe
 }
 
 if (existing) {
-    log.info(parsed_args.typeId + ' capability updated to: {}',
-            capabilityRegistry.update(existing.id(), existing.active, existing.notes(), parsed_args.capability_properties).toString()
+    log.info(parsed_args.capability_typeId + ' capability updated to: {}',
+        capabilityRegistry.update(existing.id(), existing.active, existing.notes(), parsed_args.capability_properties).toString()
     )
 }
 else {
-    log.info(parsed_args.typeId + ' capability created as: {}', capabilityRegistry.
-            add(capabilityType, true, 'configured through api', parsed_args.capability_properties).toString()
+    log.info(parsed_args.capability_typeId + ' capability created as: {}',
+        capabilityRegistry.add(capabilityType, true, 'configured through api', parsed_args.capability_properties).toString()
     )
 }
